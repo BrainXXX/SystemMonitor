7610d30890c613d1fe24e75ed9a3d4b14ca8887a
BrainX <xxxbrainxxx@gmail.com>
2022-12-27 11:44:41 +0300

    v.27.12.2022

---
+++ Form1.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SystemMonitor
+{
+    public partial class Form1 : Form
+    {
+        // Регистрируем класс KeyboardHook
+        KeyboardHook hook = new KeyboardHook();
+
+        // Блок для определения позиции курсора на экране
+        [DllImport("user32.dll")]
+        static extern bool GetCursorPos(out POINT lpPoint);
+
+        private struct POINT
+        {
+            public int X;
+            public int Y;
+        }
+
+        private POINT ShowMousePosition()
+        {
+            GetCursorPos(out POINT point);
+            return point;
+        }
+
+        // Блок для перемещения позиции курсора на экране и для клика ЛКМ
+        [DllImport("user32.dll")]
+        static extern bool SetCursorPos(int x, int y);
+
+        [DllImport("user32.dll")]
+        static extern void mouse_event(uint dwFlags, int dx, int dy, uint dwData, IntPtr dwExtraInfo);
+
+        private const uint MOUSEEVENTF_MOVE = 0x01; //Movement occurred.
+        private const uint MOUSEEVENTF_LEFTDOWN = 0x02; //The left button is down.
+        private const uint MOUSEEVENTF_LEFTUP = 0x04; //The left button is up.
+
+        int count = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            Text = "SystemMonitor v27.12.2022";
+            StartPosition = FormStartPosition.CenterScreen;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            AllowTransparency = true;
+            Opacity = 0.9;
+            BackColor = Color.AliceBlue;
+
+
+            groupBox1.Text = "GlobalPosition";
+            label1.Text = "X:";
+            label2.Text = "";
+            label3.Text = "Y:";
+            label4.Text = "";
+
+            groupBox2.Text = "JobPosition";
+            label5.Text = "X:";
+            label6.Text = "Interval:";
+            label7.Text = "Y:";
+            textBox1.Text = "";
+            textBox2.Text = "500";
+            textBox3.Text = "";
+            button1.Text = "Run [Alt+F1]";
+
+            label8.Text = count.ToString();
+            label9.Text = "Count Click:";
+            label10.Text = "[Alt+F2] - Stop; [Alt+F3] - Get position;";
+            button2.Text = "Close";
+
+            // Включаем таймер для отображения текущих координат мыши
+            timer1.Enabled = true;
+            timer1.Interval = 100;
+
+            // Регистрация события, запускающегося после ввода зарегистрированной комбинации клавиш
+            hook.KeyPressed += new EventHandler<KeyPressedEventArgs>(Hook_KeyPressed);
+            // Регистрация комбинаций клавиш
+            hook.RegisterHotKey(SystemMonitor.ModifierKeys.Alt, Keys.F1);
+            hook.RegisterHotKey(SystemMonitor.ModifierKeys.Alt, Keys.F2);
+            hook.RegisterHotKey(SystemMonitor.ModifierKeys.Alt, Keys.F3);
+        }
+
+        private void Hook_KeyPressed(object sender, KeyPressedEventArgs e)
+        {
+            if (((ModifierKeys & Keys.Alt) == Keys.Alt) && (e.Key == Keys.F1))
+            {
+                Button1_Click(sender, e);
+            }
+
+            if (((ModifierKeys & Keys.Alt) == Keys.Alt) && (e.Key == Keys.F2))
+            {
+                StopTimer();
+            }
+
+            if (((ModifierKeys & Keys.Alt) == Keys.Alt) && (e.Key == Keys.F3))
+            {
+                GetCurrentCursorPosition();
+            }
+        }
+
+        private void GetCurrentCursorPosition()
+        {
+            POINT cursorPosition = ShowMousePosition();
+
+            if (label2.Text != "" && label4.Text != "")
+            {
+                textBox1.Text = cursorPosition.X.ToString();
+                textBox3.Text = cursorPosition.Y.ToString();
+            }
+        }
+
+        private async Task LeftMouseClick(string _Xposition, string _Yposition)
+        {
+            int Xposition = Int32.Parse(_Xposition);
+            int Yposition = Int32.Parse(_Yposition);
+
+            await Task.Delay(100);
+            SetCursorPos(Xposition, Yposition);
+            await Task.Delay(100);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, 0, 0, 0, IntPtr.Zero);
+            await Task.Delay(100);
+            mouse_event(MOUSEEVENTF_LEFTUP, 0, 0, 0, IntPtr.Zero);
+            await Task.Delay(100);
+            mouse_event(MOUSEEVENTF_MOVE, 1, 1, 0, IntPtr.Zero);
+        }
+
+        private void StopTimer()
+        {
+            timer2.Enabled = false;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            POINT cursorPosition = ShowMousePosition();
+
+            if (label2.Text != cursorPosition.X.ToString() || label4.Text != cursorPosition.Y.ToString())
+            {
+                label2.Text = cursorPosition.X.ToString();
+                label4.Text = cursorPosition.Y.ToString();
+            }
+        }
+
+        private async void Timer2_Tick(object sender, EventArgs e)
+        {
+            await LeftMouseClick(textBox1.Text, textBox3.Text);
+
+            count++;
+            label8.Text = count.ToString();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+                {
+                    count = 0;
+                    timer2.Enabled = true;
+                    timer2.Interval = Int32.Parse(textBox2.Text);
+                }
+                else
+                {
+                    timer2.Enabled = false;
+                    MessageBox.Show("Не заданы параметры для курсора.", "Warning!!!");
+                }
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show(error.Source + ": " + error.ToString());
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            hook.Dispose();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанных со сборкой.
+[assembly: AssemblyTitle("SystemMonitor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SystemMonitor")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, следует установить атрибут ComVisible в TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("0ae48e23-5a15-4c97-8b24-23b2962645e6")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
